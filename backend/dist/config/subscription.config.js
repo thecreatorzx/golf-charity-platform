5993cd4db8644f53b28212acccfa2ed8d2d1df7f
thecreatorzx <saadyesican@gmail.com>
2026-04-01 02:22:11 +0530

    Updated backend

--- backend/dist/config/subscription.config.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SUBSCRIPTION_CONFIG = void 0;
-// config/subscription.config.ts
-exports.SUBSCRIPTION_CONFIG = {
-    MONTHLY_PRICE: Number(process.env.MONTHLY_PRICE || 999),
-    YEARLY_PRICE: Number(process.env.YEARLY_PRICE || 8999),
-};
